957c16643ff472d7b6123dbbdfce965e9f9cb249
Luthaf <luthaf@luthaf.fr>
2019-10-09 23:15:14 +0200

    Fix undefined lseek64 on macOS

--- gzlib.c
+++ gzlib.c
@@ -8,7 +8,7 @@
 #if defined(_WIN32) && !defined(__BORLANDC__) && !defined(__MINGW32__)
 #  define LSEEK _lseeki64
 #else
-#if defined(_LARGEFILE64_SOURCE) && _LFS64_LARGEFILE-0
+#if !defined(__APPLE__) && defined(_LARGEFILE64_SOURCE) && _LFS64_LARGEFILE-0
 #  define LSEEK lseek64
 #else
 #  define LSEEK lseek
